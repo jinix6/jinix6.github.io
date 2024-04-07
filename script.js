233112825925447edd545c653a65c483eac67e33
Crystal Person <135565126+jinix6@users.noreply.github.com>
2024-04-08 02:16:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,16 @@ document.getElementById("gt").addEventListener("click", function() {
 window.open("https://github.com/jinix6");});
 document.getElementById("icn_li").addEventListener("click", function() {
 window.open("https://jinix6.github.io/Icon/");});
+document.getElementById("icn_grup").addEventListener("click", function() {
+window.open("https://t.me/FFinfox");});
+function h() {
+  setTimeout(function() {
+  var loaderBg = document.getElementById("lb");
+  loaderBg.style.opacity = "0";
+  setTimeout(function() {
+    loaderBg.style.visibility = "hidden";
+  }, 500);
+}, 1000);
+}
+
 
